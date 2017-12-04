6f6c9eaceb58970a0a14ec6a6d96fd53a5f7d98f
raghavgr94 <raghavgr94@gmail.com>
2017-12-04 15:57:12 +0300

    :wrench: pep8 flip 2D matrix

--- flip_2d_matrix.py
+++ flip_2d_matrix.py
@@ -1,27 +1,30 @@
 """
-You are given an m x n 2D image matrix (List of Lists) 
-where each integer represents a pixel. 
+You are given an m x n 2D image matrix (List of Lists)
+where each integer represents a pixel.
 Flip it in-place along its horizontal axis.
 
 Example:
 
-Input image :  
+Input image :
               1 1
-              0 0 
-Modified to :   
+              0 0
+Modified to :
               0 0
               1 1
 """
 def flip_horizontal_axis(matrix):
+    """
+    Flip 2D matrix
+    """
     # # [[1, 1], [0, 0]]
     # tempRow = matrix[0]
     # matrix[0] = matrix[1]
     # matrix[1] = tempRow
-    
     if len(matrix[0]) < 2:
         return
-    
-    for iRow in range(len(matrix) / 2):
-        tempRow = matrix[iRow]
-        matrix[iRow] = matrix[len(matrix) - 1 - iRow]
-        matrix[len(matrix) - 1 - iRow] = tempRow
+
+    for i_row in range(len(matrix) / 2):
+        tempRow = matrix[i_row]
+        matrix[i_row] = matrix[len(matrix) - 1 - i_row]
+        matrix[len(matrix) - 1 - i_row] = tempRow
+        
